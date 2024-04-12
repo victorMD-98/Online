673a979cbc672960218223bcc83c2af6dc32c5c1
Victor <victor246257@gmail.com>
2024-04-12 18:53:24 +0200

    mod

--- Online/resources/views/layouts/app.blade.php
+++ Online/resources/views/layouts/app.blade.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+  
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
